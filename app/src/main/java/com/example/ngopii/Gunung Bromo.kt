b9cd3cad30955058963c9af74e86a81efdf0256e
Barleyan <iyan.manisrejo@gmail.com>
2024-06-04 20:30:14 +0700

    update data binding

--- app/src/main/java/com/example/ngopii/Gunung Bromo.kt
+++ app/src/main/java/com/example/ngopii/Gunung Bromo.kt
@@ -2,15 +2,21 @@ package com.example.ngopii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.ngopii.databinding.ActivityGunungbromoBinding
 
-class Bromo : AppCompatActivity() {
+class GunungBromo : AppCompatActivity() {
+    private lateinit var binding: ActivityGunungbromoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`activity_gunungbromo`)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_gunungbromo)
+
+        binding.title = "Welcome to Gunung Bromo"
 
         val bar = supportActionBar
         bar!!.title = "Gunung Bromo"
         bar.setDisplayHomeAsUpEnabled(true)
-
     }
+
 }
